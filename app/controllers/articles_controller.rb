68a8ff1e50d7014863810447eaa1dc789484d7df
Ola Ajibode <ola.ajibode@gmail.com>
2015-12-19 18:41:43 +0000

    error added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,8 +20,16 @@ end
     def create 
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-    redirect_to article_show(@article)
+        if @article.save
+           flash[:notice] = "Article was successfully created" 
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+        
+        # @article = Article.new(article_params)
+        # @article.save
+    # redirect_to article_show(@article)
     end
     
     

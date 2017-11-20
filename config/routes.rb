05b47766f151f57dd863b8200aa7b094c621e86c
Ola Ajibode <ola.ajibode@gmail.com>
2017-11-20 21:29:20 +0000

    user scaffolds added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :comments
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -44,8 +47,7 @@ resources :articles
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
